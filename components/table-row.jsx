12ad80854bcffe30d42d23f95bb01878192fccba
Michaela Hunt <micapplehunt@gmail.com>
2025-07-04 13:23:39 -0400

    now built for android

--- components/table-row.jsx
+++ components/table-row.jsx
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
         display: 'grid', 
         flexDirection: 'row',
         borderColor: 'black',
-        borderWidth: '1',
+        borderWidth: 1,
         width: '100%',
     },
     tText: {
